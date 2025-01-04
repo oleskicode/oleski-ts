bf1c2c643a0c6e20786bbad23c7f973a58656657
Oleksii Plaunov <plaunov@gmail.com>
2025-01-04 16:00:00 +0200

    big cleanup

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # oleski-ts
-# oleski-ts
--- src/hello_world.ts
+++
@@ -1,9 +0,0 @@
-let message : string = "Hello world!"
-
-console.log(message)
-
-function logMessage(message: string) {
-    console.log(message)
-}
-
-logMessage("log me!")
