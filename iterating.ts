ea9c46955afcd9b3cd2be1c7ec0e20c4c57955ed
Oleksii Plaunov <plaunov@gmail.com>
2025-01-07 13:15:35 +0200

    iterating.ts

---
+++ iterating.ts
@@ -0,0 +1,23 @@
+// 1. through string sing for-of
+const greetingString = "Hi how are you?"
+
+for (const char of greetingString) { 
+    console.log(char)
+}
+
+// 2. convert string to Array and forEach
+Array.from(greetingString).forEach((char) => { 
+    console.log("---" + char)
+})
+
+// 3. through array using forEach
+let fruits: string[] = ["apple", "banana", "coconut"]
+
+fruits.forEach( (fruit) => { // using forEach
+    console.log("fruits.forEach: " + fruit)
+})
+
+// 4. through array using "for of"
+for (let fruit of fruits) { // using for-of
+    console.log("for-of: " + fruit)
+}
