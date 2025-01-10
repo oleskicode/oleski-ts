659e6573cd7a5c2ba2c72a488ec80765229dde01
Oleksii Plaunov <plaunov@gmail.com>
2025-01-10 15:19:53 +0200

    fixed

--- sort_users_1.ts
+++ sort_users_1.ts
@@ -1,14 +1,14 @@
-// There is an array of random numbers. The task is to return an array with gender ("female"/"male") based on whether the second-to-last digit of each number in the array is even or odd.
+// The task is to return an array with "female"/"male" based on whether the second-to-last (!) digit of each number in the array is even or odd.
 // Input: (1234, 12345)
 // Result: ("female", "male")
 
-const inputArray = [1234, 12345, 55, 66, 7, 0]
+const inputArray = [1234, 12345, 57, 67]
 console.log(inputArray)
 
 // using forEach
 let resultArray: string [] = []
 inputArray.forEach((number) => {
-    if (number % 2 === 0) {
+    if (Math.floor(number / 10) % 2 === 0) {
         resultArray.push("male")
     } else {
         resultArray.push("female")
@@ -18,7 +18,7 @@ console.log(resultArray)
 
 // using map
 let mappedArray = inputArray.map((number) => {
-    if ( number % 2 === 0) {
+    if ( Math.floor(number / 10) % 2 === 0) {
         return "male"
     } else {
         return "female"
