ba66109a84b56610ee81793cec2e1a30fd3d10fa
Oleksii Plaunov <plaunov@gmail.com>
2025-01-14 18:14:03 +0200

    capitalize words

---
+++ string_capitalize_words.ts
@@ -0,0 +1,16 @@
+// capitalize the first letter of each word in a string.
+
+function capitalizeEachWord(inputString: string): string {
+    let outputString = ""
+    let array1 = inputString.split(" ")
+
+    array1.forEach((word) => {
+        word = word.charAt(0).toUpperCase() + word.slice(1)        
+        // console.log(word)
+        outputString += word + " "
+    })
+
+    return outputString.trim() // trim last space
+}
+
+console.log(capitalizeEachWord('some string exercise'));
