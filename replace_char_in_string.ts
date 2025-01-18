037ab9d9f1472f8f50dec9fddb4e216641831934
Oleksii Plaunov <plaunov@gmail.com>
2025-01-18 15:16:47 +0200

    replaceChar

---
+++ replace_char_in_string.ts
@@ -0,0 +1,13 @@
+// function to replace specific char in string
+
+function replaceChar(inputString: string, index: number, newChar: string) {
+    let outputString = inputString
+
+    outputString = inputString.slice(0, index) + newChar + inputString.slice(index + 1 )
+
+    return outputString 
+}
+
+console.log(replaceChar("epple", 0, "a"))
+console.log(replaceChar("bonana", 1, "a"))
+console.log(replaceChar("coconud", 6, "t"))
