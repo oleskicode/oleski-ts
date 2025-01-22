37aede1961fadc36f56e63de3842004af438c2ac
Oleksii Plaunov <plaunov@gmail.com>
2025-01-22 15:01:28 +0200

    string to array

---
+++ string_to_array.ts
@@ -0,0 +1,7 @@
+// Write function to split a string and convert it into an array of words.
+
+function string_to_array(str: string): string[] {
+    return str.split(" ")
+}
+
+console.log(string_to_array("Robin Hood")); // ["Robin", "Hood"]
