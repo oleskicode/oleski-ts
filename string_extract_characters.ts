c3608e48e6c49dff4a732b3c208099e660add3e6
Oleksii Plaunov <plaunov@gmail.com>
2025-01-22 15:26:02 +0200

    minor impr

--- string_extract_characters.ts
+++ string_extract_characters.ts
@@ -12,3 +12,22 @@ function truncate_string(str: string, truncateNumber?: number) : string {
 
 console.log(truncate_string("Apple")); // "A"
 console.log(truncate_string("Robin Hood", 5)); // "Robi"
+
+// same but using array and for
+function truncate_string2(str: string, truncateNumber?: number) : string {
+    if (truncateNumber === undefined) {
+        truncateNumber = 1
+    }
+
+    const inputArray = str.split("")
+    let outputString = ""
+
+    for (let i = 0; i < truncateNumber; i++ ) {
+        outputString += inputArray[i]
+    }
+
+    return outputString
+}
+
+console.log(truncate_string2("Apple")); // "A"
+console.log(truncate_string2("Robin Hood", 5)); // "Robi"
