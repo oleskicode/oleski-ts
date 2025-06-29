6bfeef214e219c12dede0a80680441fda733aa5a
Oleksii Plaunov <plaunov@gmail.com>
2025-06-29 18:29:50 +0300

    destructuring example

---
+++ desctucturing.ts
@@ -0,0 +1,12 @@
+const patrick = {
+  name: "Patrick",
+  surname: "Star",
+  salary: 103,
+};
+
+console.log(patrick);
+
+const { surname, salary } = patrick; // Destructuring
+
+console.log(surname);
+console.log(salary);
